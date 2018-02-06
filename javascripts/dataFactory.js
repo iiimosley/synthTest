18c087a8a22dc859af315d68a91a11d6ea2a4c1b
Trey Mosley <iiimosley@gmail.com>
2018-02-06 10:00:19 -0600

    user patch template generate; loads patch upon click

--- javascripts/dataFactory.js
+++ javascripts/dataFactory.js
@@ -1,6 +1,20 @@
 'use strict';
 const $ = require('jquery');
 
+module.exports.savePatch = (patchData) => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: 'https://synthulx.firebaseio.com/patches.json',
+            method: 'POST',
+            data: JSON.stringify(patchData)
+        })
+            .done(patch => {
+                console.log(patch);
+                resolve(patch);
+            });
+    });
+};
+
 module.exports.getPatches = (uid) => {
     return new Promise((resolve, reject) => {
         $.ajax({ url: `https://synthulx.firebaseio.com/patches.json?orderBy="uid"&equalTo="${uid}"` })
@@ -14,23 +28,16 @@ module.exports.getPatches = (uid) => {
     });
 };
 
+module.exports.loadPatch = (pKey) => {
+    return new Promise((resolve, reject) => {
+        $.ajax({ url: `https://synthulx.firebaseio.com/patches/${pKey}.json` })
+            .done(patch => resolve(patch));
+    });
+};
+
 module.exports.setPatch = () => {
     return new Promise((resolve, reject) => {
         $.ajax({url: '../patchData.json'})
         .done( patch => resolve(patch));
-    });
-};
-
-module.exports.savePatch = (patchData) => {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: 'https://synthulx.firebaseio.com/patches.json',
-            method: 'POST',
-            data: JSON.stringify(patchData)
-        })
-        .done(patch => {
-            console.log(patch);
-            resolve(patch);
-        });
     });
 };
