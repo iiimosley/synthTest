7ce2727041e7ef2bbfe899c7008c0ec1e2faebf6
Trey Mosley <iiimosley@treys-mbp.attlocal.net>
2018-02-05 22:40:10 -0600

    reset key id for patch objects

--- javascripts/authFactory.js
+++ javascripts/authFactory.js
@@ -14,13 +14,7 @@ module.exports.logout = () => firebase.auth().signOut();
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         DataFactory.getPatches(user.uid)
-            .then(userPatches =>{
-                let patchKeys = Object.keys(userPatches);
-                patchKeys.map(key=>{
-                    userPatches[key].patch_id = key;
-                });
-                view.userAuth(userPatches);
-            });
+        .then(userPatches =>view.userAuth(userPatches));
     } else {
         console.log('not logged in');
         view.noUser();
