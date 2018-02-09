03c18c5e1947b934a38ddd47e953effa384b06fa
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2018-02-09 09:56:22 -0600

    full functioning adsr graph

--- javascripts/main.js
+++ javascripts/main.js
@@ -232,6 +232,7 @@ $(document).on("click", "#deletePatch", function () {
 
 
 function draw() {
+    $("#chartCrtl").trigger("change");
     let canvas = document.getElementById("adsr");
     let ctx = canvas.getContext("2d");
     ctx.beginPath();
@@ -239,7 +240,10 @@ function draw() {
     $("#chartCtrl").on("input", function(){
         let aVal = +($("#attack").val()*100);
         let dVal = +(canvas.height) - +($("#decay").val() * +(canvas.height));
+        let sVal = +(canvas.height) - +($("#sustain").val() * +(canvas.height));
+        let rVal = +(canvas.width) - (+($("#release").val()*100));
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         ctx.beginPath();
         ctx.moveTo(0, canvas.height);
         ctx.lineTo(+aVal, +dVal);
@@ -247,8 +251,31 @@ function draw() {
         ctx.stroke();
         ctx.fillStyle = 'rgb(255,250,250)';
         ctx.fillRect(+aVal, +dVal, 5, 5);
+
+        ctx.beginPath();
+        ctx.moveTo(+aVal, +dVal);
+        ctx.lineTo(130, +sVal);
+        ctx.strokeStyle = 'rgb(255,250,250)';
+        ctx.stroke();
+        ctx.fillStyle = 'rgb(255,250,250)';
+        ctx.fillRect(130, +sVal, 5, 5);
+
+        ctx.beginPath();
+        ctx.moveTo(130, +sVal);
+        ctx.lineTo(+rVal, +sVal);
+        ctx.strokeStyle = 'rgb(255,250,250)';
+        ctx.stroke();
+        ctx.fillStyle = 'rgb(255,250,250)';
+        ctx.fillRect(+rVal, +sVal, 5, 5);
+
+        ctx.beginPath();
+        ctx.moveTo(+rVal, +sVal);
+        ctx.lineTo(canvas.width, canvas.height);
+        ctx.strokeStyle = 'rgb(255,250,250)';
+        ctx.stroke();
+        ctx.fillStyle = 'rgb(255,250,250)';
+        // ctx.fillRect(130, +sVal, 5, 5);
     });
-    // $("#chartCrtl").trigger("change");
 }
 
 draw();
