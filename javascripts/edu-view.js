c0540f4fcf186e2b66eafeb537151450f10662e3
Trey Mosley <iiimosley@treys-mbp.attlocal.net>
2018-02-11 16:51:46 -0600

    osc section of synthbuilder: details, outline on click

--- javascripts/edu-view.js
+++ javascripts/edu-view.js
@@ -1,11 +1,19 @@
 'use strict';
 const $ = require('jquery');
 const osc = require('../templates/osc.hbs');
+const oscDetail = require('../templates/osc-detail.hbs');
 
 module.exports.startBuild = () => {
     $("#eduModal").empty();
     $("#eduModal").append(osc);
     $("#eduModal").show();
+};
+
+module.exports.printOscDetail = (wave) => {
+    let oscObj = {};
+    oscObj[wave] = true;
+    $("#oscDetail").empty();
+    $("#oscDetail").append(oscDetail(oscObj));
 };
 
 // module.exports.closeBuild = (patchName, bool) => {
